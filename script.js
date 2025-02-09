181fc962a100da3940eb858405e43d6599b197af
LuukvanHees <l.vanhees@havesdigital.com>
2025-02-09 19:28:17 +0100

    Deploy function to log human input
    Add getHumanChoice function to prompt users to enter a valid text value.

--- script.js
+++ script.js
@@ -2,13 +2,11 @@
 console.log("Hello World")
 
 /* DETERMINE a random number */
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max)
-}
 
-/* PRINT text based on a numerical value */
+
+/* DETERMINE a random numerical value*/
 function computerChoice(){
-    let text;
+    let text = Math.random;
     /* IF random numerical value is below 0.33 THEN return rock */
     if (Math.random() <0.33){
         text = "Rock";
@@ -21,6 +19,36 @@ function computerChoice(){
     else {
         text = "Scissors"
     }
+    /* PRINT computer choice */
     console.log(text)
 }
+
 computerChoice()
+
+function getHumanChoice(){
+    let choice = prompt ("Choose rock, paper, or scissors");
+    /* IF user input value is equal to rock THEN return rock */
+    if (choice.toLowerCase() === "rock"){
+        choice = "Rock"
+        alert("You've chosen rock")
+    }
+    /* IF user input value is equal to paper THEN return paper */
+    else if (choice.toLowerCase() === "paper"){
+        choice = "Paper"
+        alert("You've chosen paper")
+    }
+    /* IF user input value is equal to scissors THEN return scissors */
+    else if (choice.toLowerCase() === "scissors"){
+        choice = "Scissors"
+        alert("You've chosen scissors")
+    }
+     /* ELSE return invalid input */
+     else {
+       alert("You've entered invalid input")
+       getHumanChoice()
+    }
+    /* PRINT human choice */
+    console.log(choice)
+}
+
+getHumanChoice()
